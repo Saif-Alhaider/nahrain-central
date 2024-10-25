5246d9ab9fd2bdbe65f1ee318aecaa6c4b50894d
saif.harith <saifharith90@gmail.com>
2024-10-25 17:51:43 +0300

    add: login screen design

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}","./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens:{
+        'windows': { 'raw': '(min-height: 705px)' },
+      },
+      colors:{
+        primary: 'rgba(var(--primary))',
+        background: 'rgba(var(--background))',
+        card: 'rgba(var(--card))',
+        secondary: 'rgba(var(--secondary))',
+        onBackground: 'rgba(var(--on-background))',
+        selectedItem: 'rgba(var(--selected-item))',
+        strokeGrey: 'rgba(var(--stroke-grey))',
+        logo: 'rgba(var(--logo))',
+        onBackgroundCaption: 'rgba(var(--on-background-caption))',
+        error: 'rgba(var(--error))',
+        onPrimary: 'rgba(var(--on-primary))',
+      },
+    },
+  },
+  plugins: [],
+}
