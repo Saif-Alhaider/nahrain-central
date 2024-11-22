1f390c115f7e0d991990ed952c11621e26d96f1d
said.harith <saifharith90@gmail.com>
2024-11-22 16:04:37 +0300

    add: recorded lectures screen for student

--- src/presentation/App.js
+++ src/presentation/App.js
@@ -6,6 +6,7 @@ import {GradesScreen} from "./Prof/Grades/GradesScreen";
 import {MainScaffold} from "./Common/component/MainScaffold";
 import {SettingsScreen} from "./Common/screens/Settings/SettingsScreen";
 import {NotFound404} from "./Common/screens/NotFound404/NotFound404";
+import {StudentRecordedLectures} from "./Student/StudentRecordedLectures/StudentRecordedLectures";
 
 
 function App() {
@@ -18,7 +19,9 @@ function App() {
                 <Route path='/'>
                     <Route path='/nahrain-central' element={<MainScaffold/>}>
                         <Route index={true} element={<div></div>}/>
+                        <Route path='recorded-lectures' element={<StudentRecordedLectures/>}/>
                         <Route path='grades' element={<GradesScreen/>}/>
+                        <Route path='settings' element={<SettingsScreen/>}/>
                         <Route path='settings' element={<SettingsScreen/>}/>
                     </Route>
                     <Route path='*' element={<NotFound404/>}/>
