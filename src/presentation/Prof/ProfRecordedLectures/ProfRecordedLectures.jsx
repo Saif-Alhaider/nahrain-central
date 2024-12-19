e0dc40e239bc5c8d15f5509d5279a6f6f1fc09e0
saif.harith <saifharith90@gmail.com>
2024-12-19 22:45:21 +0300

    add: use flex-wrap

--- src/presentation/Prof/ProfRecordedLectures/ProfRecordedLectures.jsx
+++ src/presentation/Prof/ProfRecordedLectures/ProfRecordedLectures.jsx
@@ -7,14 +7,14 @@ import React from "react";
 
 export const ProfRecordedLectures = () => {
     return (<div className={"w-full h-full p-6"}>
-        <div className="w-full flex flex-row justify-between">
-            <div className={"flex flex-row gap-2"}>
-                <DropDown text={"All"} items={["First Course","Second Course",]}/>
-                <DropDown text={"Lab"} items={["Theoretical"]}/>
-            </div>
+        <div className="w-full flex flex-wrap gap-4 justify-between">
             <button
                 className="bg-primary text-onPrimary text-nowrap rounded-lg text-xl px-4 py-2 ">Publish a Lecture
             </button>
+            <div className={"flex flex-row gap-2"}>
+                <DropDown text={"All"} items={["First Course", "Second Course",]}/>
+                <DropDown text={"Lab"} items={["Theoretical"]}/>
+            </div>
         </div>
         <div
             className="grid  sm:grid-cols-1 md:grid-cols-[repeat(auto-fill,minmax(268px,1fr))] gap-6 mt-6 pb-6">
