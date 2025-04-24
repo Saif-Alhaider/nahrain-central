6425ba0235f8f240313c33558b4d95abee982025
Saif Alhaider <saifharith90@gmail.com>
2025-04-24 16:54:04 +0300

    add: support for english arabic language in home screen

--- src/presentation/Prof/Home/HomeScreen.jsx
+++ src/presentation/Prof/Home/HomeScreen.jsx
@@ -1,15 +1,17 @@
+import {useTranslation} from "react-i18next";
+
 export const HomeScreen = () => {
+    const [t] = useTranslation("global");
     return (
         <div className={`size-full bg-background p-6`}>
             <div className={`flex flex-col gap-2`}>
-                <h1 className={`font-semibold text-[32px] text-onBackground`}>Home Dashboard</h1>
-                <p className={` text-[20px] text-onBackgroundCaption`}> Manage users, schedules, absences, and grades in
-                    one place.</p>
+                <h1 className={`font-semibold text-[32px] text-onBackground`}>{t("dashboard")}</h1>
+                <p className={` text-[20px] text-onBackgroundCaption`}>{t("dashboard_description")}</p>
             </div>
             <div className="grid md:!flex md:flex-wrap mt-12 grid-cols-[repeat(auto-fit,minmax(175px,1fr))] gap-2 ">
                 <ExamCard className="max-w-[277px] cursor-pointer"/>
-                <ViewAllCard className="max-w-[277px] grow cursor-pointer" title="Total Students" number="1,245"/>
-                <ViewAllCard className="max-w-[277px] grow cursor-pointer" title="Total Professors" number="58"/>
+                <ViewAllCard className="max-w-[277px] grow cursor-pointer" title={t("total_students")} number="1,245"/>
+                <ViewAllCard className="max-w-[277px] grow cursor-pointer" title={t("total_profs")} number="58"/>
             </div>
         </div>
     )
@@ -17,10 +19,11 @@ export const HomeScreen = () => {
 
 
 const ExamCard = ({className}) => {
+    const [t] = useTranslation("global");
     return (
         <div className={`${className} rounded-2xl bg-secondary p-4`}>
-            <h2 className="hidden md:!block text-lg font-semibold text-onPrimary">Upcoming Exams Final</h2>
-            <h2 className="block md:!hidden text-4xl font-semibold text-onPrimary">Finals</h2>
+            <h2 className="hidden md:!block text-lg font-semibold text-onPrimary">{t("upcoming_exams_final")}</h2>
+            <h2 className="block md:!hidden text-4xl font-semibold text-onPrimary">{t("finals")}</h2>
             <p className="mt-2 md:!text-md text-2xl text-onBackground dark">Dec 20, 2024</p>
             <p className="mt-4 hidden md:!block text-sm text-onPrimary">
                 Ensure all schedules and materials are uploaded in advance.
